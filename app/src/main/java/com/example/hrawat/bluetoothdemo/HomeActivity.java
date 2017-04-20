9384f7ab3fe7b71caba3b7227bc6fa5bf90983ba
Harish Rawat <hrawat@merkur.local>
2017-04-20 10:11:07 +0530

    Second commit

--- app/src/main/java/com/example/hrawat/bluetoothdemo/HomeActivity.java
+++ app/src/main/java/com/example/hrawat/bluetoothdemo/HomeActivity.java
@@ -15,6 +15,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //Second Commit
     }
 
     private void checkForBluetooth(){
